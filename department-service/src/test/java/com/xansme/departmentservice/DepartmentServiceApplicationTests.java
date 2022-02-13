678996e37380b61eb7b6399f649bd4e9ff8c0329
Mulia Ichsan <mulia.ichsan@amarbank.co.id>
2022-02-13 11:08:23 +0700

    add 1: department-services

---
+++ department-service/src/test/java/com/xansme/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xansme.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
