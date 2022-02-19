9bd0db68de5e6c52feb62973c563236244b08107
Mulia Ichsan <mulia.ichsan@amarbank.co.id>
2022-02-19 16:32:13 +0700

    remove unwanted service

--- user-service2/src/test/java/com/xansme/userservice/UserServiceApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.xansme.userservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UserServiceApplicationTests {
-}
